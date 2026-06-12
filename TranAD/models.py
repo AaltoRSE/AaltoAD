53ddc266fc7594a4ef356f6219777e150927fb0f
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-06-12 13:46:12 +0300

    Update experiments

--- TranAD/models.py
+++ TranAD/models.py
@@ -17,6 +17,27 @@ from TranAD.stagnn_core import STAGNN_Core
 torch.manual_seed(1)
 
 
+# Base class for models
+class BaseModel(nn.Module):
+	def __init__(self, feats, learning_rate=0.002, epochs=5, weight_decay=1e-5):
+		super(BaseModel, self).__init__()
+		self.name = 'BaseModel'
+		self.lr = learning_rate
+		self.epochs = epochs
+		self.weight_decay = weight_decay
+		self.n_feats = feats
+
+	def forward(self, x):
+		return x
+
+	def train_step(self, epoch, data, optimizer, scheduler, feats):
+		return 0.0, optimizer.param_groups[0]['lr']
+
+	def eval_step(self, epoch, data, optimizer, scheduler, feats):
+		return np.zeros_like(data), np.zeros_like(data)
+
+
+
 ## Separate LSTM for each variable
 class LSTM_Univariate(nn.Module):
 	def __init__(self, feats, n_hidden=1, n_layers=1, learning_rate=0.002, batch_size = 512, epochs=5, weight_decay=1e-5):
