fe34e1e284ce49964ff9efbac83925e5cfe6cdf3
Rantaharju Jarno <jarno.rantaharju@aalto.fi>
2026-06-15 11:40:01 +0300

    Fix linting issues

--- TranAD/models/__init__.py
+++ TranAD/models/__init__.py
@@ -1,46 +1,48 @@
 import torch
+
+from .attention import Attention
+from .base import BaseModel
+from .cae_m import CAE_M
+from .dagmm import DAGMM
+from .gdn import GDN
+from .lstm_ad import LSTM_AD
+from .lstm_ae import LSTM_AE
+from .lstm_univariate import LSTM_Univariate
+from .mad_gan import MAD_GAN
+from .mscred import MSCRED
+from .mtad_gat import MTAD_GAT
+from .omni_anomaly import OmniAnomaly
+from .stagnn import STAGNN
+from .tranad import TranAD
+from .tranad_adversarial import TranAD_Adversarial
+from .tranad_base import TranADBase
+from .tranad_basic import TranAD_Basic
+from .tranad_selfconditioning import TranAD_SelfConditioning
+from .tranad_transformer import TranAD_Transformer
+from .usad import USAD
+
 torch.manual_seed(1)
 
-from .base import BaseModel
-from .tranad_base import TranADBase
-from .lstm_univariate import LSTM_Univariate
-from .attention import Attention
-from .lstm_ae import LSTM_AE
-from .lstm_ad import LSTM_AD
-from .dagmm import DAGMM
-from .omni_anomaly import OmniAnomaly
-from .usad import USAD
-from .mscred import MSCRED
-from .cae_m import CAE_M
-from .mtad_gat import MTAD_GAT
-from .gdn import GDN
-from .mad_gan import MAD_GAN
-from .tranad_basic import TranAD_Basic
-from .tranad_transformer import TranAD_Transformer
-from .tranad_adversarial import TranAD_Adversarial
-from .tranad_selfconditioning import TranAD_SelfConditioning
-from .tranad import TranAD
-from .stagnn import STAGNN
 
 __all__ = [
-	"BaseModel",
-	"TranADBase",
-	"LSTM_Univariate",
-	"Attention",
-	"LSTM_AE",
-	"LSTM_AD",
-	"DAGMM",
-	"OmniAnomaly",
-	"USAD",
-	"MSCRED",
-	"CAE_M",
-	"MTAD_GAT",
-	"GDN",
-	"MAD_GAN",
-	"TranAD_Basic",
-	"TranAD_Transformer",
-	"TranAD_Adversarial",
-	"TranAD_SelfConditioning",
-	"TranAD",
-	"STAGNN",
+    "BaseModel",
+    "TranADBase",
+    "LSTM_Univariate",
+    "Attention",
+    "LSTM_AE",
+    "LSTM_AD",
+    "DAGMM",
+    "OmniAnomaly",
+    "USAD",
+    "MSCRED",
+    "CAE_M",
+    "MTAD_GAT",
+    "GDN",
+    "MAD_GAN",
+    "TranAD_Basic",
+    "TranAD_Transformer",
+    "TranAD_Adversarial",
+    "TranAD_SelfConditioning",
+    "TranAD",
+    "STAGNN",
 ]
